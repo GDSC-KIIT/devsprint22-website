f34d5ad6aac7acfc5abece5d913b6f98335dd722
Nikhil George <thegeorgenikhil@gmail.com>
2022-10-08 21:30:06 +0530

    chore: font fetching to _document.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,6 @@ import { Main, Navbar } from "../components";
 export default function Home() {
   return (
     <>
-      <Head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=clash-display@200,400,700,500,600,300&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <div id="landing-page">
         <Navbar />
         <Main />
