9fa2f63a41362b7103808888947684061e289550
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-10-09 14:48:06 +0530

    add: landing page + images (#2)
    * add: landing page + images
    
    Co-authored-by: Nikhil George <thegeorgenikhil@gmail.com>

--- components/index.js
+++ components/index.js
@@ -1,2 +1,4 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Example } from "./Example/Example";
+export { Main } from "./Main/Main";
+export { ApplyWithDevfolioBtn } from "./ApplyWithDevfolioBtn/ApplyWithDevfolioBtn";
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,13 @@
 import Head from "next/head";
-import { Navbar } from "../components";
+import { Main, Navbar } from "../components";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=clash-display@200,400,700,500,600,300&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Navbar />
+      <div id="landing-page">
+        <Navbar />
+        <Main />
+      </div>
     </>
   );
 }
