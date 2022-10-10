7e8a40208e35fa8c9b721992cbc05e8ab7b6ed0b
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2022-10-10 10:53:06 +0530

    add: sponsor section (#5)
    * add: sponsor section
    
    Co-authored-by: Nikhil George <thegeorgenikhil@gmail.com>

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 export { Navbar } from "./Navbar/Navbar";
-export { Example } from "./Example/Example";
 export { Main } from "./Main/Main";
 export { ApplyWithDevfolioBtn } from "./ApplyWithDevfolioBtn/ApplyWithDevfolioBtn";
 export { About } from "./About/About";
+export { Sponsors } from "./Sponsors/Sponsors";
