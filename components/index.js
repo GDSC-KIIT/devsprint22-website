c613a86c229902dd00ec462af0cc7e4857183ee5
Nikhita <bnikhita2003@gmail.com>
2022-10-12 01:11:10 +0530

    default exports

--- components/index.js
+++ components/index.js
@@ -3,3 +3,8 @@ export { Main } from "./Main/Main";
 export { ApplyWithDevfolioBtn } from "./ApplyWithDevfolioBtn/ApplyWithDevfolioBtn";
 export { About } from "./About/About";
 export { Sponsors } from "./Sponsors/Sponsors";
+export { Tracks } from "./Tracks/Tracks";
+export { Timeline } from "./Timeline/Timeline";
+export { Footer } from "./Footer/Footer";
+export { Rules } from "./Rules/Rules";
+export { Faq } from "./FAQ/Faq";
